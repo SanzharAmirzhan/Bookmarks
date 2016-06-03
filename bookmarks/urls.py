04ba720fcaafddf5c1755d65e237a8da502b1202
SanzharAmirzhan <amir.sanzhar@gmail.com>
2016-06-03 17:39:56 +0600

    added images list

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     url('social-auth/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^images/', include("images.urls", namespace="images")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
